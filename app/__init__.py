4be806a7565c1e00d0462a8e33081a29c8bf4e1f
Maria Obregon Garcia <mobregong@gmail.com>
2022-02-13 16:57:28 -0500

    Creates firestore_routes that take uid and ics url and store parsed events details in firesotre (under users collection/user doc/user_info subcollection/events document)

--- app/__init__.py
+++ app/__init__.py
@@ -4,10 +4,15 @@ from flask_migrate import Migrate
 from dotenv import load_dotenv
 import os
 from flask_cors import CORS
+from google.cloud import firestore
+from firebase_admin import credentials, firestore, initialize_app
 
 # db = SQLAlchemy()
 # migrate = Migrate()
 load_dotenv()
+cred = credentials.Certificate('key.json')
+default_app = initialize_app(cred)  
+db = firestore.client()
 
 
 def create_app():
@@ -15,24 +20,18 @@ def create_app():
 
     app.url_map.strict_slashes = False
     
-    # app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-    # app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
-    #     "SQLALCHEMY_DATABASE_URI")
-
-    # # Import models here for Alembic setup
-    # from app.models.board import Board
-    # from app.models.card import Card
     
 
-    # db.init_app(app)
-    # migrate.init_app(app, db)
-
+    # db = firestore.client()
+    # collection = db.collection('users')
     # # Register Blueprints here
     # from .routes.board_routes import board_bp
     # app.register_blueprint(board_bp)
     from .routes.calendar import calendar_bp
     app.register_blueprint(calendar_bp)
+
+    from .routes.firestore_routes import firestore_bp
+    app.register_blueprint(firestore_bp)
 
     # from .routes.card_routes import card_bp
     # app.register_blueprint(card_bp)
--- app/routes/firestore_routes.py
+++ app/routes/firestore_routes.py
@@ -12,7 +12,7 @@ import requests
 collection = db.collection('users')
 # doc = collection.document('AM4Y8EdoOQVeHZL7z11DNmvZ4LZ2')
 
-calendar_bp = Blueprint('calendar', __name__, url_prefix="/calendars")
+firestore_bp = Blueprint('firestore', __name__, url_prefix="/firestore")
 
 # # Get calendar link
 # @calendar_bp.route("", methods=["GET"])
@@ -28,15 +28,26 @@ calendar_bp = Blueprint('calendar', __name__, url_prefix="/calendars")
 #     # return jsonify(c)
 #     return make_response(calendar, 200)
 
-# Get events from link (for dev)
-@calendar_bp.route("/events", methods=["GET"])
-def get_events():
+
+# Get events from firestore
+@firestore_bp.route("", methods=["GET"])
+def get_firestore():
     request_body = request.get_json()
     url = request_body["url"]
     user = request_body["uid"]
     doc = collection.document(user)
+    res = doc.get().to_dict()
+
+    return make_response(res, 200)
+
+# Test post to firestore
+@firestore_bp.route("", methods=["PUT"])
+def post_to_users():
+    request_body = request.get_json()
+    url = request_body["url"]
+    user = request_body["uid"]
+
     c = Calendar(requests.get(url).text)
-    # e = list(c.timeline)[1]
 
     events_dict = {}
     i = 1
@@ -52,23 +63,12 @@ def get_events():
             location = "virtual"
         events_dict[event.name] = [{"start_time": start},{"end_time": end},{"location":location},{"description":description}]
         i += 1
-    
-    # return jsonify(c)
-    return make_response(events_dict, 200)
 
-# Get events from firestore
-@calendar_bp.route("/firestore", methods=["GET"])
-def get_firestore():
-    request_body = request.get_json()
-    url = request_body["url"]
-    user = request_body["uid"]
     doc = collection.document(user)
-    res = doc.get().to_dict()
+    res = doc.update(events_dict)
+    return make_response("posted", 200)
 
-    return make_response(res, 200)
-
-# Test post to firestore
-@calendar_bp.route("/firestore", methods=["PUT"])
+@firestore_bp.route("events", methods=["PUT"])
 def post_to_subcollection():
     request_body = request.get_json()
     url = request_body["url"]
@@ -91,34 +91,6 @@ def post_to_subcollection():
         events_dict[event.name] = [{"start_time": start},{"end_time": end},{"location":location},{"description":description}]
         i += 1
 
-    doc = collection.document(user)
-    res = collection.document(user).update(events_dict)
+    doc = collection.document(user).collection('user_info').document('events')
+    res = doc.set(events_dict)
     return make_response("posted", 200)
-
-# Post events to firestore
-@calendar_bp.route("/events", methods=["POST"])
-def post_events():
-    request_body = request.get_json()
-    url = request_body["url"]
-    user = request_body["uid"]
-    doc = collection.document(user)
-    c = Calendar(requests.get(url).text)
-    # e = list(c.timeline)[1]
-
-    events_dict = {}
-    i = 1
-    for event in c.events:
-        start = str(event.begin)
-        end = str(event.end)
-        if not event.location:
-            location = "None"
-        else:
-            location = event.location
-        description = event.description
-        if ".com" in description:
-            location = "virtual"
-        events_dict[event.name] = [{"start_time": start},{"end_time": end},{"location":location},{"description":description}]
-        i += 1
-    
-    # return jsonify(c)
-    return make_response(events_dict, 200)
